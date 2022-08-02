36e47d1c5415c4f7ae04f602a55a1252dd6e5c4e
yuhangdo yuhangdo <wangyuh111@163.com>
2022-08-02 16:43:07 +0800

    write menu and playground scene

---
+++ game/static/js/src/playground/zbase.js
@@ -0,0 +1,19 @@
+class AcGamePlayground {
+    constructor(root) {
+        this.root = root;
+        this.$playground = $(`<div>游戏界面</div>`);
+        this.hide();
+        this.root.$ac_game.append(this.$playground);
+
+        this.start();
+    }
+
+    start(){
+    }
+    show(){
+        this.$playground.show();
+    }
+    hide(){  //关闭playground界面
+        this.$playground.hide();
+    }
+}
--- game/views/index.py
+++ game/views/index.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "multiends/web.`html")
+    return render(request, "multiends/web.html")
